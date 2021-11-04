ff5abb6197b4adbf2f6be3545ec69489a4a1aaa5
RikSeg <riksegatto@gmail.com>
2021-11-03 23:34:04 -0300

    Initial commit

---
+++ src/test/java/com/github/rikseg/CitiesAPI/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rikseg.CitiesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
