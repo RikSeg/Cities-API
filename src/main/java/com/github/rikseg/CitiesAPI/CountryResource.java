0239675fd1f139c465d527f8e301dd7b84d7d073
RikSeg <riksegatto@gmail.com>
2021-11-04 17:20:52 -0300

    API complete, with a Pageable function and Hibernate SQL logs

--- src/main/java/com/github/rikseg/CitiesAPI/CountryResource.java
+++ src/main/java/com/github/rikseg/CitiesAPI/CountryResource.java
@@ -4,11 +4,12 @@ package com.github.rikseg.CitiesAPI;
 import com.github.rikseg.CitiesAPI.countries.Country;
 import com.github.rikseg.CitiesAPI.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
-import java.util.Scanner;
+
 
 @RestController
 @RequestMapping("/countries") //on localhost:8080/countries it will bring all countries on Database's "Pais" table in json format
@@ -18,7 +19,8 @@ public class CountryResource {
     private CountryRepository repository;
 
     @GetMapping
-    public List<Country> countries() {
-            return repository.findAll();
+    public Page<Country> countries(Pageable page) {
+
+        return repository.findAll(page);
     }
 }
