4fa4d4a8a62664954a68e23aaa20e12083821648
RikSeg <riksegatto@gmail.com>
2021-11-05 17:21:46 -0300

    Added Exception treatment at Service functions

--- src/main/java/com/github/rikseg/CitiesAPI/cities/DistanceResource.java
+++ src/main/java/com/github/rikseg/CitiesAPI/cities/DistanceResource.java
@@ -4,6 +4,7 @@ package com.github.rikseg.CitiesAPI.cities;
 import com.github.rikseg.CitiesAPI.cities.service.DistanceService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,14 +24,14 @@ public class DistanceResource {
     }
 
     @GetMapping("/points_calc")
-    public Double byPoints(@RequestParam(name = "from") final Long city1, @RequestParam(name = "to") final Long city2){
+    public ResponseEntity byPoints(@RequestParam(name = "from") final Long city1, @RequestParam(name = "to") final Long city2){
         log.info("byPoints");
-        return service.distanceByPointsInMiles(city1,city2);
+        return ResponseEntity.ok().body(service.distanceByPointsInMiles(city1,city2));
     }
 
     @GetMapping("/cube_calc")
-    public Double byCube(@RequestParam(name = "from") final Long city1, @RequestParam(name = "to") final Long city2){
+    public ResponseEntity byCube(@RequestParam(name = "from") final Long city1, @RequestParam(name = "to") final Long city2){
         log.info("byCube");
-        return service.distanceByCubeInMeters(city1,city2);
+        return ResponseEntity.ok().body(service.distanceByCubeInMeters(city1,city2));
     }
 }
